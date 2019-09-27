ee9061b481ee70e4016d1af033af47bf156c5181
Hailey J Gaylor <48165448+Hgaylor0220@users.noreply.github.com>
2019-09-27 15:46:03 -0700

    staring on the classes for the bakery

---
+++ bakery.cs
@@ -0,0 +1,10 @@
+using System;
+namespace Bakery
+{
+    class Goods 
+    {
+    public int Price { get; set;}
+    public int Quanity { get; set;}
+    
+    }
+}
--- project.cs
+++ project.cs
@@ -1,8 +1,11 @@
 using System;
+using Bakery;
 
-class Program
+public class Program
 {
-  static void Main()
+ public static void Main()
   {
+    Console.WriteLog("Hello! Welcome to Pierre's Bakery were we have Pastrys and Bread specials on the daily! would you like to take a look around? [Y for yes or N for no] ");
+    string lookaround = Console.Readline( );
   }
 }
