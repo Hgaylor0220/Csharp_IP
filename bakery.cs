011e05d8e6c4ac37aaf0bbc1e5fef3e6556b52ca
Hailey J Gaylor <48165448+Hgaylor0220@users.noreply.github.com>
2019-09-27 18:11:23 -0700

    completed backend logic to return cost to user

--- bakery.cs
+++ bakery.cs
@@ -4,23 +4,51 @@ namespace Products
     class Pastrys 
     {
         private int Price = 2;
-        private int PriceBreak = 3;
+        private int PriceBreakAmount = 3;
         private int PriceBreakPrice = 5;
 
-        public Pastrys (int quantity) {
-            Console.WriteLine("You want " + quantity + " pastires.");
+        public int quantity;
+
+        public Pastrys (int orderQuantity) {
+            this.quantity = orderQuantity;
+            Console.WriteLine("You want " + orderQuantity + " pastries.");
         }
 
-        public ReturnCost (int quanity){
-            int cost = 5;
+        public int ReturnCost (){
+            int priceBreakTotal = Decimal.ToInt32(this.quantity / PriceBreakAmount);
+            Console.WriteLine("priceBreakTotal: " + priceBreakTotal);
+            int regularTotal = this.quantity - (priceBreakTotal * PriceBreakAmount);
+            Console.WriteLine("regularTotal: " + regularTotal);
+
+            int cost = (regularTotal * this.Price) + (priceBreakTotal * this.PriceBreakPrice);
+            Console.WriteLine("cost: " + cost);
             return cost;
         }
     
     }
-    // class bread
-    // {
-    //     public int Price { get; set; }
-    //     public int Quanity { get; set; }
+    class Bread
+    {
+        private int Price = 5;
+        private int PriceBreakAmount = 3;
+        private int PriceBreakPrice = 10;
 
-    // }
+        public int quantity;
+
+        public Bread(int orderQuantity)
+        {
+            this.quantity = orderQuantity;
+            Console.WriteLine("You want " + orderQuantity + " loaves of bread.");
+        }
+
+        public int ReturnCost()
+        {
+            // taking the price break and dividing it by the quantity of bread req. to calculate cost
+            int priceBreakTotal = Decimal.ToInt32(this.quantity / PriceBreakAmount);
+            int regularTotal = this.quantity - (priceBreakTotal * PriceBreakAmount);
+
+            int cost = (regularTotal * this.Price) + (priceBreakTotal * this.PriceBreakPrice); // calculating the total cost
+            return cost;
+        }
+
+    }
 }
--- project.cs
+++ project.cs
@@ -5,28 +5,27 @@ public class Program
 {
  public static void Main()
   {
-    // Bread customerBreadOrder = new Bread(price,quanity);
-    // Pastry customerPastryOrder = new Pastry(Predicate,quanity);
-
+   
     Console.WriteLine("Hello! Welcome to Pierre's Bakery! We have Pastries and Bread specials on the daily! We have bread for buy two get one free at $5 per loaf and pastries 3 for $5 or $2 each, would you like to make a purchase? [Y for yes or N for no] ");
     string Lookaround = Console.ReadLine();
-      if(Lookaround.ToLower() == "y")
-      {
-        Console.WriteLine("Wonderful!!! How many loafs of bread would you like,They are Buy 2, get 1 free. A single loaf costs $5?");
-        int UserBreadOrder = Console.ReadLine();
-        Console.WriteLine("And how many pastries would you like? They are 3 for $5 or $2 each.");
-        int UserPastryOrder = Console.ReadLine();
 
-        Pastrys pastry_order = new Products.Pastrys(UserPastryOrder);
-        Bread bread_order = new Products.Bread(UserBreadOrder);
+    if(Lookaround.ToLower() == "y")
+    {
+      Console.WriteLine("Wonderful!!! How many loafs of bread would you like,They are Buy 2, get 1 free. A single loaf costs $5?");
+      string UserBreadOrder = Console.ReadLine();
+      Console.WriteLine("And how many pastries would you like? They are 3 for $5 or $2 each.");
+      string UserPastryOrder = Console.ReadLine();
 
-        int total = pastry_order.ReturnCost() + bread_order.ReturnCost();
+      Pastrys pastry_order = new Products.Pastrys(Convert.ToInt32(UserPastryOrder));
+      Bread bread_order = new Products.Bread(Convert.ToInt32(UserBreadOrder));
 
-        Console.WriteLine("Your total comes to: $" + total);
-      }
-      else
-      {
-        Console.WriteLine("Thank you for stopping buy! Please come visit us again");
-      } 
-  } 
+      int total = pastry_order.ReturnCost() + bread_order.ReturnCost();
+
+      Console.WriteLine("Your total comes to: $" + total);
+    }
+    else
+    {
+      Console.WriteLine("Thank you for stopping buy! Please come visit us again");
+    }
+  }
 }
