4e2cddef6693e7e020c06a2d49677ab2927a55ae
Hailey J Gaylor <48165448+Hgaylor0220@users.noreply.github.com>
2019-09-27 17:01:42 -0700

    added backend logic to calculate total cost

--- bakery.cs
+++ bakery.cs
@@ -1,16 +1,26 @@
 using System;
-namespace products
+namespace Products
 {
-    class pastrys 
+    class Pastrys 
     {
-    public int Price { get; set;}
-    public int Quanity { get; set;}
+        private int Price = 2;
+        private int PriceBreak = 3;
+        private int PriceBreakPrice = 5;
+
+        public Pastrys (int quantity) {
+            Console.WriteLine("You want " + quantity + " pastires.");
+        }
+
+        public ReturnCost (int quanity){
+            int cost = 5;
+            return cost;
+        }
     
     }
-    class bread
-    {
-        public int Price { get; set; }
-        public int Quanity { get; set; }
+    // class bread
+    // {
+    //     public int Price { get; set; }
+    //     public int Quanity { get; set; }
 
-    }
+    // }
 }
